b3f2ff541863bc6251f014c9868867c500b14849
Pankaj Sharma <sharmapankaj2512@gmail.com>
2022-05-17 19:03:56 +0400

    multiple links

--- tests/test_links.py
+++ tests/test_links.py
@@ -2,8 +2,8 @@ import dataclasses
 import unittest
 
 
-def lint_links(param):
-    return NoWarning()
+def lint_links(text):
+    return text.count("](") * [NoWarning()]
 
 
 @dataclasses.dataclass
@@ -13,7 +13,11 @@ class NoWarning:
 
 class LinksTestCase(unittest.TestCase):
     def test_square_brackets_followed_by_parenthesis(self):
-        self.assertEqual(NoWarning(), lint_links("[Duck Duck Go](https: // duckduckgo.com)"))
+        self.assertEqual([NoWarning()], lint_links("[Duck Duck Go](https: // duckduckgo.com)"))
+
+    def test_multiple_links(self):
+        self.assertEqual([NoWarning(), NoWarning()], lint_links(
+            "[Duck Duck Go](https: // duckduckgo.com) [Duck Duck Go](https: // duckduckgo.com)"))
 
 
 if __name__ == '__main__':
