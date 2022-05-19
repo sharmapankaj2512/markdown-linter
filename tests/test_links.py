e678afc4c3fc0651f59bd2f52967ea65feb030f7
Pankaj Sharma <sharmapankaj2512@gmail.com>
2022-05-19 07:53:17 +0400

    fix links

--- tests/test_links.py
+++ tests/test_links.py
@@ -6,14 +6,14 @@ from linter_warning import NoWarning
 
 class LinksTestCase(unittest.TestCase):
     def test_square_brackets_followed_by_parenthesis(self):
-        self.assertEqual([NoWarning()], lint_links("[Duck Duck Go](https: // duckduckgo.com)"))
+        self.assertEqual([NoWarning()], lint_links("[Duck Duck Go](https://duckduckgo.com)"))
 
     def test_multiple_links(self):
         self.assertEqual([NoWarning(), NoWarning()], lint_links(
-            "[Duck Duck Go](https: // duckduckgo.com) [Duck Duck Go](https: // duckduckgo.com)"))
+            "[Duck Duck Go](https: // duckduckgo.com) [Duck Duck Go](https://duckduckgo.com)"))
 
     def test_missing_square_brackets(self):
-        self.assertEqual([MissingSquareBrackets()], lint_links("(https: // duckduckgo.com)"))
+        self.assertEqual([MissingSquareBrackets()], lint_links("(https://duckduckgo.com)"))
 
     def test_missing_parenthesis(self):
         self.assertEqual([MissingParenthesis()], lint_links("[Duck Duck Go]"))
